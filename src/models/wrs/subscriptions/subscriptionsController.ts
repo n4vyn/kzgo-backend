53e0ea393e27e15049050b4480b8fe37479e082f
Vojta Navratil <navratil.v@hanaboso.com>
2022-06-16 09:16:53 +0200

    yeet zod, fixing sub endpoints

--- src/models/wrs/subscriptions/subscriptionsController.ts
+++ src/models/wrs/subscriptions/subscriptionsController.ts
@@ -1,67 +1,48 @@
 import express from 'express'
-import { z } from 'zod'
 import { SubscriptionRepo } from './SubscriptionRepo'
 
 const router = express.Router()
 
-const inputSchema = z.object({
-  endpoint: z.string(),
-  expirationTime: z.string(),
-  keys: z.object({
-    p256dh: z.string(),
-    auth: z.string(),
-  }),
-})
-
 router.post('/subscribe', async (req, res) => {
-  const sub = await inputSchema.safeParseAsync(req.body)
+  const sub = req.body
 
-  if (!sub.success) {
+  // don't really need validation, if sub is invalid it will just get removed in 10 mins
+  if (!sub.endpoint) {
     res.sendStatus(400)
     return
   }
 
-  SubscriptionRepo.insertOne(sub.data)
-    .then(() => {
-      res.sendStatus(201)
-    })
-    .catch(error => {
-      if (error.code === 11000) {
-        //duplicate error, do nothing?
-        res.sendStatus(200)
-      } else {
-        throw error
-      }
-    })
+  try {
+    await SubscriptionRepo.insertOne(sub)
+    res.sendStatus(201)
+  } catch (error) {
+    if (error.code === 11000) {
+      //duplicate error, do nothing?
+      res.sendStatus(200)
+    } else {
+      throw error
+    }
+  }
 })
 
 router.post('/unsubscribe', async (req, res) => {
-  const sub = await inputSchema.safeParseAsync(req.body)
+  const sub = req.body
 
-  if (!sub.success) {
-    res.sendStatus(400)
-    return
-  }
-
-  SubscriptionRepo.deleteByEndpoint(sub.data.endpoint)
-    .then(() => {
-      res.sendStatus(201)
-    })
+  await SubscriptionRepo.deleteByEndpoint(sub.endpoint)
+  res.sendStatus(204)
 })
 
 router.post('/checkSubStatus', async (req, res) => {
-  const sub = await inputSchema.safeParseAsync(req.body)
+  const sub = req.body
 
-  if (!sub.success) {
+  if (!sub.endpoint) {
     res.sendStatus(400)
     return
   }
 
-  SubscriptionRepo.findByEndpoint(sub.data.endpoint)
-    .then(s => {
-      if (s) res.json(s)
-      else res.sendStatus(404)
-    })
+  const s = await SubscriptionRepo.findByEndpoint(sub.endpoint)
+  if (s) res.json(s)
+  else res.sendStatus(404)
 })
 
 export default router
